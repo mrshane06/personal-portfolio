3221938971bbbdd835e2f0641bfde74d648e394e
mrshane06 <niekerkkeegan@007gmail.com>
2024-09-19 14:18:22 +0200

    edu

--- src/components/EducationComp.vue
+++ src/components/EducationComp.vue
@@ -1,6 +1,9 @@
 <template>
     <div>
-        
+        <slot name="education"></slot>
+        <slot name="image"></slot>
+        <slot name="eduDesc"></slot>
+        <slot name="years"></slot>
     </div>
 </template>
 <script>
--- src/views/EducationView.vue
+++ src/views/EducationView.vue
@@ -1,13 +1,59 @@
 <template>
     <div>
-        
+        <h1>Education</h1>
+        <section  class="edu" v-for="education in educationData()" :key="education">
+            <education-comp>
+                <template #education>
+               <h3>{{education.education}}</h3>
+              </template>
+              <br>
+              <template #image>
+                <img :src='education.image' class="eduImg">
+              </template>
+              <br>
+              <template #eduDesc>
+                <h4>{{ education.eduDesc }}</h4>
+              </template>
+              <br>
+              <template #years>
+                <h4> years : {{education.years}}</h4>
+              </template>
+            </education-comp>
+        </section>
     </div>  
 </template>
 <script>
+import EducationComp from '@/components/EducationComp.vue';
 export default {
-    
+    components:{
+        EducationComp
+    },
+    methods: {
+        educationData(){
+            return this.$store.state.education
+        },
+    },
+    computed:{
+        getData(){
+            return this.$store.dispatch ('getData')
+        }
+    },
+    mounted(){
+        this.getData
+    }
 }
 </script>
 <style>
-    
+    h1{
+        width: 100px;
+        padding-left: 160px;
+    }
+    .eduImg{
+        width: 80px;
+        height: 80px;
+    }
+    .edu{
+        width: 300px;
+        padding-left: 80px;
+    }
 </style>
